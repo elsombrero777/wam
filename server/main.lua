d4b8721d68969a5902d0db00995a5791218b7f15
elsombrero777 <102491331+elsombrero777@users.noreply.github.com>
2022-03-28 19:59:24 +0100

    Added BuyAMarketplace Logic

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,19 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
 lib.versionCheck('elsombrero777/wam')
 if not lib.checkDependency('ox_lib', '2.0.2') then error('You don\'t have latest version of ox_lib') end
 if not lib.checkDependency('ox_inventory', '2.6.0') then error('You don\'t have latest version of ox_inventory') end
 
+--------------[ WAM ]--------------
 
+RegisterNetEvent('wam:checkMoney')
+AddEventHandler('wam:checkMoney', function(id, label, slots, weight, owner)
+    local p = ESX.GetPlayerFromId(source)
+    local money = p.getAccount('bank').money
+
+    if money > Config.Price then
+        p.removeAccountMoney('bank', Config.Price)
+        exports.ox_inventory:RegisterStash(id, label, slots, weight, owner)
+    end
+end)
