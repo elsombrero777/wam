91a6220d22df312e8136df19edb873032299f242
N-fire <55892717+N-fire@users.noreply.github.com>
2022-03-28 16:44:24 +0200

    feat(client): add Market marker logic

--- client/main.lua
+++ client/main.lua
@@ -1,7 +1,18 @@
-local coords = GetEntityCoords(PlayerPedId())
-local market = lib.points.new(coords, 7, { inZone = true})
+Citizen.CreateThread(function()
+    for i = 1, #Config.Locations, 1 do
+        local BlackMarketPoint = lib.points.new(Config.Locations[i].pos, 7, { type = Config.Locations[i].type})
+    end
+    
 
+end)
 
-function market:nearby()
-    DrawMarker(29, 463.0681, -770.5978, 27.34216, 0, 0, 0, 0, 0, 0, 0.75, 0.75, 0.75, 31, 94, 255, 155, 0, 0, 2, 1, 0, 0, 0);
+function BlackMarketPoint:nearby()
+    DrawMarker(29,self.coords.x, self.coords.y, self.coords.z, 0, 0, 0, 0, 0, 0, 0.75, 0.75, 0.75, 31, 94, 255, 155, 0, 0, 2, 1, 0, 0, 0);
+    if self.currentDistance < 1 and IsControlJustReleased(0, 38) then
+        OpenMarket(self.type)
+    end
+end
+
+function OpenMarket(type)
+    print('marketOpen : '..type)
 end
