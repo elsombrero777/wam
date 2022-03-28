ffeb5e79f6a9d7e8ec4363e12d7ef5446b26b94c
N-fire <55892717+N-fire@users.noreply.github.com>
2022-03-28 16:52:07 +0200

    fix(client): Market marker logic

--- client/main.lua
+++ client/main.lua
@@ -1,18 +1,14 @@
-Citizen.CreateThread(function()
-    for i = 1, #Config.Locations, 1 do
-        local BlackMarketPoint = lib.points.new(Config.Locations[i].pos, 7, { type = Config.Locations[i].type})
-    end
-    
+for i = 1, #Config.Locations, 1 do
+    local MarketPoint = lib.points.new(Config.Locations[i].pos, 7, { type = Config.Locations[i].type})
+end
 
-end)
-
-function BlackMarketPoint:nearby()
+function MarketPoint:nearby()
     DrawMarker(29,self.coords.x, self.coords.y, self.coords.z, 0, 0, 0, 0, 0, 0, 0.75, 0.75, 0.75, 31, 94, 255, 155, 0, 0, 2, 1, 0, 0, 0);
     if self.currentDistance < 1 and IsControlJustReleased(0, 38) then
-        OpenMarket(self.type)
+        OpenMarket(self.id ,self.type)
     end
 end
 
-function OpenMarket(type)
-    print('marketOpen : '..type)
+function OpenMarket(id ,type)
+    print('marketOpen : '..id..' type : '..type)
 end
